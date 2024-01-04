b94b58bd45922a50446258664c2c0fae08fe794e
HaiNguyen <vincent.hainguyenq@gmail.com>
2024-01-04 10:05:41 +0700

    Hai: update x1 sdk

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -2,7 +2,7 @@ import JSBI from 'jsbi';
 export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
     MAINNET = 42766,
-    TESTNET = 7001
+    TESTNET = 195
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,8 +79,8 @@ export const WETH = {
     ChainId.TESTNET,
     addresses[ChainId.TESTNET].WETH,
     18,
-    'WZETA',
-    'Wrapped ZETA',
-    'https://zetachain-athens-3.blockscout.com/'
+    'WOKB',
+    'Wrapped OKB',
+    'https://www.oklink.com/'
   )
 }
