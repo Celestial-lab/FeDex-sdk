8146bda1597cc9a73c28e20a581da7c5a80fd3d6
HaiNguyen <vincent.hainguyenq@gmail.com>
2024-04-10 09:28:13 +0700

    Hai: update code

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -71,9 +71,9 @@ export const WETH = {
     ChainId.MAINNET,
     addresses[ChainId.MAINNET].WETH,
     18,
-    'WUSDC',
-    'Wrapped USDC',
-    'https://scan.zkfair.io/'
+    'WOKB',
+    'Wrapped OKB',
+    'https://www.okx.com/explorer/xlayer'
   ),
   [ChainId.TESTNET]: new Token(
     ChainId.TESTNET,
